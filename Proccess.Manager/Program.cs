40e7c3d0ab64f2cd0a1145a610703e1fb72c4e57
hamidramezanali <hamid.ra2018@gmail.com>
2022-12-23 22:22:39 +0100

    Deserialization

--- Proccess.Manager/Program.cs
+++ Proccess.Manager/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Processes.Manager;
 using System.Diagnostics;
+using System.Text.Json;
 
 //var result = await Cli.Wrap(@"C:\Users\hamid\source\repos\Proccess.Manager\Process.Counter\bin\Debug\net6.0\Process.Counter.exe")
 //    //.WithArguments("--foo bar")
@@ -10,7 +12,7 @@ using System.Diagnostics;
 
 //* Create your Process
 Process process = new Process();
-process.StartInfo.FileName = @"C:\Users\hamid\source\repos\Proccess.Manager\Process.Counter\bin\Debug\net6.0\Process.Counter.exe";
+process.StartInfo.FileName = @"C:\Users\hamid\source\repos\Proccess.Manager\ProcessManager.ManageServices\bin\Debug\net6.0\ProcessManager.ManageServices.exe";
 process.StartInfo.Arguments = "/c DIR";
 process.StartInfo.UseShellExecute = false;
 process.StartInfo.RedirectStandardOutput = true;
@@ -23,13 +25,21 @@ process.Start();
 process.BeginOutputReadLine();
 process.BeginErrorReadLine();
 process.WaitForExit();
-Console.ReadLine();
+
 
 
 static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
 {
-    //* Do your stuff with the output (write to console/log/StringBuilder)
-    Console.WriteLine(outLine.Data);
-    //var weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(outLine.Data);
-    //Console.WriteLine(weatherForecast.Date.ToString());
+    try
+    {
+        //* Do your stuff with the output (write to console/log/StringBuilder)
+        Console.WriteLine(outLine.Data);
+        var weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(outLine.Data);
+        Console.WriteLine(weatherForecast.Date.ToString());
+    }
+    catch (Exception)
+    {
+
+    }
+
 }
--- ProcessManager.ManageServices/Program.cs
+++ ProcessManager.ManageServices/Program.cs
@@ -1,29 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.Diagnostics;
+﻿using ProcessManager.ManageServices;
+using System.Text.Json;
 
-Console.WriteLine("Hello, World!");
-//* Create your Process
-Process process = new Process();
-process.StartInfo.FileName = @"C:\Users\hamid\source\repos\Proccess.Manager\Process.Counter\bin\Debug\net6.0\Process.Counter.exe";
-process.StartInfo.Arguments = "/c DIR";
-process.StartInfo.UseShellExecute = false;
-process.StartInfo.RedirectStandardOutput = true;
-process.StartInfo.RedirectStandardError = true;
-//* Set your output and error (asynchronous) handlers
-process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
-process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
-//* Start process and handlers
-process.Start();
-process.BeginOutputReadLine();
-process.BeginErrorReadLine();
-process.WaitForExit();
-Console.ReadLine();
-
-
-static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+var weatherForecast = new WeatherForecast
 {
-    //* Do your stuff with the output (write to console/log/StringBuilder)
-    Console.WriteLine(outLine.Data);
-    //var weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(outLine.Data);
-    //Console.WriteLine(weatherForecast.Date.ToString());
+    Date = DateTime.Parse("2019-08-01"),
+    TemperatureCelsius = 25,
+    Summary = "Hot"
+};
+string jsonString = JsonSerializer.Serialize(weatherForecast);
+for (int i = 0; i < 5; i++)
+{
+    //Console.WriteLine(@$"{i}Hello, World!");
+    Console.WriteLine(jsonString);
+    Task.Delay(500).Wait();
 }
